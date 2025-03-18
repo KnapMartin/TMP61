2e5c7b175a41758004e20ad9a1126a6a002b7b08
KnapMartin <martin_knap@yahoo.com>
2025-03-18 09:25:34 +0100

    implemented

---
+++ TMP61.c
@@ -0,0 +1,29 @@
+/*
+ * TMP61.c
+ *
+ *  Created on: Mar 17, 2025
+ *      Author: knap-linux
+ */
+
+#include "TMP61.h"
+
+void TMP61_init(TMP61 *self, TMP61_AdcRes adcRes, const uint32_t voltageBias_mV, const uint32_t resistanceBias_Ohm)
+{
+	self->m_adcRes = adcRes;
+	self->m_voltageBias_mV = voltageBias_mV;
+	self->m_resistanceBias_Ohm = resistanceBias_Ohm;
+	self->m_state = TMP61_STATE_INIT;
+}
+
+float TMP61_get_temperature(TMP61 *self, const uint32_t adcReading, const uint32_t vref_mV)
+{
+	if (self->m_state != TMP61_STATE_INIT) return 0.0;
+
+	// calculate divider voltage out
+	uint32_t voltage_mV = ((float)adcReading / (float)self->m_adcRes) * vref_mV;
+	// calculate resistance (voltage divider)
+	uint32_t resistance_Ohm = (voltage_mV * self->m_resistanceBias_Ohm) / (self->m_voltageBias_mV - voltage_mV);
+	// get temperature, use 2nd order polynomial approximation
+	float temperature_C = resistance_Ohm * resistance_Ohm * TMP61_COEF_0 + resistance_Ohm * TMP61_COEF_1 + TMP61_COEF_2;
+	return temperature_C;
+}
---
+++ TMP61.h
@@ -0,0 +1,46 @@
+/*
+ * TMP61.h
+ *
+ *  Created on: Mar 17, 2025
+ *      Author: knap-linux
+ */
+
+#ifndef INC_TMP61_H_
+#define INC_TMP61_H_
+
+#include <stdint.h>
+
+#define TMP61_COEF_0 (float)-4.948869300752372e-07
+#define TMP61_COEF_1 (float)0.02626742751078931
+#define TMP61_COEF_2 (float)-188.33938786524962
+
+typedef enum
+{
+	TMP61_ST_NONE = -1,
+	TMP61_STATE_INIT,
+	TMP61_STATE_ERROR,
+} TMP61_State;
+
+typedef enum
+{
+	TMP61_ADC_RES_NONE = -1,
+	TMP61_ADC_RES_8_BIT = 255,
+	TMP61_ADC_RES_10_BIT = 1023,
+	TMP61_ADC_RES_12_BIT = 4095,
+	TMP61_ADC_RES_14_BIT = 16383,
+	TMP61_ADC_RES_16_BIT = 65535,
+} TMP61_AdcRes;
+
+typedef struct
+{
+	TMP61_State m_state;
+	TMP61_AdcRes m_adcRes;
+	uint32_t m_voltageBias_mV;
+	uint32_t m_resistanceBias_Ohm;
+} TMP61;
+
+void TMP61_init(TMP61 *self, TMP61_AdcRes adcRes, const uint32_t voltageBias_mV, const uint32_t resistanceBias_Ohm);
+float TMP61_get_temperature(TMP61 *self, const uint32_t adcReading, const uint32_t vref_mV);
+
+
+#endif /* INC_TMP61_H_ */
